3917483f74e1e8fdf6aab6c57563a2e9decd48d0
Mohamed Yusuf <mohamed208.y@proton.me>
2023-08-20 12:15:13 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    current = []
+      
+    if(length == 0):
+        print(current)
+        return
+    
+    for i in range(length):
+        if i == 0 or i == 1:
+            current.append(i)
+        else:
+            current.append(current[i - 2] + current[i-1])
+       
+    print(current)
